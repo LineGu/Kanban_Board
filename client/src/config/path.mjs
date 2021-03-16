14c93ccb07f724405d0f6ee199f182f2875d4469
Kang-Hyun-Gu <kanghg1116@naver.com>
2021-03-16 16:43:01 +0900

    [Kanban] change ip to fix nginx error
    nginx해결을 위해 path의 ip를 수정해주었다.

--- client/src/config/path.mjs
+++ client/src/config/path.mjs
@@ -1,5 +1,5 @@
 export const PATH = {
-  main_page: 'http://127.0.0.1:3000/kanban',
-  login_page: 'http://127.0.0.1:3000/',
-  server_url: 'http://127.0.0.1:8080',
+  main_page: 'http://101.101.217.111:3000/kanban',
+  login_page: 'http://101.101.217.111:3000/',
+  server_url: 'http://101.101.217.111:8080',
 };
