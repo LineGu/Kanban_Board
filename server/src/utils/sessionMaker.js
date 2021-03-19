077cfd50ffd1c2205c44f69e9f81a2c1ea406f05
Kang-Hyun-Gu <kanghg1116@naver.com>
2021-03-19 21:48:21 +0900

    chage to https

--- server/src/utils/sessionMaker.js
+++ server/src/utils/sessionMaker.js
@@ -1,11 +1,9 @@
-const cookieOptions = require('../options/cookie.js');
 const sessionMaker = {
   createSession(session, isLogined, name, userId, loginMethod) {
     session.isLogined = isLogined;
     session.name = name;
     session.userId = userId;
     session.loginMethod = loginMethod;
-    session.cookie = cookieOptions;
   },
 };
 
