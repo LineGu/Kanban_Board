a6cc25a4cae045649ede128ef5d931d6561f911d
Kang-Hyun-Gu <kanghg1116@naver.com>
2021-03-19 18:40:12 +0900

    [kanban] chage path to use main page in nginx
    nginx에서 main page를 사용하기 위해 경로의 포트를 변경해주었다.

--- client/src/config/path.mjs
+++ client/src/config/path.mjs
@@ -1,5 +1,5 @@
 export const PATH = {
-  main_page: 'http://101.101.217.111:3000/main',
+  main_page: 'http://101.101.217.111:3001/',
   login_page: 'http://101.101.217.111:3000/',
   server_url: 'http://101.101.217.111:8080',
 };
