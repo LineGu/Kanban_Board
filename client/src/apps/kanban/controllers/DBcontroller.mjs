a58d86e5e177be71d1cca4c1138c36720b02b9b4
Kang-Hyun-Gu <kanghg1116@naver.com>
2021-03-19 18:55:02 +0900

    [Refactor] chage path and refactor kanban/controllers/DBcontroller.mjs
    kanban/controllers/DBcontroller.mjs을 리팩토리하고 경로를 수정하였다.

--- client/src/apps/kanban/controllers/DBcontroller.mjs
+++ client/src/apps/kanban/controllers/DBcontroller.mjs
@@ -1,7 +1,10 @@
+import { PATH } from '../../../config/path.mjs';
+import { ServerStatus } from '../../../config/serverStatus.mjs';
+
 export const DBcontroller = {
   async deleteContainer(idOfDeleteContainer) {
     try {
-      const result = await fetch(`http://localhost:8080/user/container/${idOfDeleteContainer}`, {
+      const result = await fetch(PATH.server_url + `/user/container/${idOfDeleteContainer}`, {
         method: 'DELETE',
         credentials: 'include',
       });
@@ -19,7 +22,7 @@ export const DBcontroller = {
   },
   async addContainer(idOfAddContainer, name, containerIndex, userId = 0) {
     try {
-      const result = await fetch(`http://localhost:8080/user/container/${idOfAddContainer}`, {
+      const result = await fetch(PATH.server_url + `/user/container/${idOfAddContainer}`, {
         method: 'POST',
         body: JSON.stringify({
           name,
@@ -43,7 +46,7 @@ export const DBcontroller = {
   },
   async updateContainer(idOfContainerUpdated, name) {
     try {
-      const result = await fetch(`http://localhost:8080/user/container/${idOfContainerUpdated}`, {
+      const result = await fetch(PATH.server_url + `/user/container/${idOfContainerUpdated}`, {
         method: 'PATCH',
         body: JSON.stringify({
           name,
@@ -66,18 +69,15 @@ export const DBcontroller = {
 
   async moveContainer(idOfContainerMoved, indexOfStart, indexToGo) {
     try {
-      const result = await fetch(
-        `http://localhost:8080/user/container/index/${idOfContainerMoved}`,
-        {
-          method: 'PATCH',
-          body: JSON.stringify({
-            indexOfStart,
-            indexToGo,
-          }),
-          headers: { 'Content-Type': 'application/json' },
-          credentials: 'include',
-        },
-      );
+      const result = await fetch(PATH.server_url + `/user/container/index/${idOfContainerMoved}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+          indexOfStart,
+          indexToGo,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+      });
 
       if (result.status !== 200) {
         throw new Error(response.status);
@@ -94,7 +94,7 @@ export const DBcontroller = {
   async deleteCard(idOfContainer, idOfCardToDelete) {
     try {
       const result = await fetch(
-        `http://localhost:8080/user/card/${idOfContainer}/${idOfCardToDelete}`,
+        PATH.server_url + `/user/card/${idOfContainer}/${idOfCardToDelete}`,
         {
           method: 'DELETE',
           credentials: 'include',
@@ -115,21 +115,18 @@ export const DBcontroller = {
 
   async addCard(idOfContainer, idOfCardToAdd, header, body, footer, cardIndex, userId = 0) {
     try {
-      const result = await fetch(
-        `http://localhost:8080/user/card/${idOfContainer}/${idOfCardToAdd}`,
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            header,
-            body,
-            footer,
-            cardIndex,
-            userId,
-          }),
-          headers: { 'Content-Type': 'application/json' },
-          credentials: 'include',
-        },
-      );
+      const result = await fetch(PATH.server_url + `/user/card/${idOfContainer}/${idOfCardToAdd}`, {
+        method: 'POST',
+        body: JSON.stringify({
+          header,
+          body,
+          footer,
+          cardIndex,
+          userId,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+      });
 
       if (result.status !== 200) {
         throw new Error(response.status);
@@ -146,7 +143,7 @@ export const DBcontroller = {
   async updateCard(idOfContainerUpdated, idOfCardUpdated, header, body) {
     try {
       const result = await fetch(
-        `http://localhost:8080/user/card/${idOfContainerUpdated}/${idOfCardUpdated}`,
+        PATH.server_url + `/user/card/${idOfContainerUpdated}/${idOfCardUpdated}`,
         {
           method: 'PATCH',
           body: JSON.stringify({
@@ -173,7 +170,7 @@ export const DBcontroller = {
   async moveCard(idOfContainerMoved, idOfCardMoved, indexsOfStart, indexsToGo) {
     try {
       const result = await fetch(
-        `http://localhost:8080/user/card/index/${idOfContainerMoved}/${idOfCardMoved}`,
+        PATH.server_url + `/user/card/index/${idOfContainerMoved}/${idOfCardMoved}`,
         {
           method: 'PATCH',
           body: JSON.stringify({
